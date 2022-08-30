ffd91e4cd94be4b575cdbcdc008ed42b00cc1be0
YousefLanister <112448840+YousefLanister@users.noreply.github.com>
2022-08-30 20:30:44 +0300

    Add files via upload

---
+++ design.js
@@ -0,0 +1,28 @@
+//    [a]
+document.getElementById("a_in_header").innerHTML = `YOUSEF HAMZA`;
+//    [h2]
+document.getElementById("h2_in_cards").innerHTML = `Services`;
+document.getElementById("h2_in_projects").innerHTML = `Projects`;
+document.getElementById("h2_3").innerHTML = `Le’s Worke together`;
+//    [h3]
+document.getElementById("h3_in_main").innerHTML = `I Build Life Changing Courses`;
+document.getElementById("h3_in_info").innerHTML = `Script Writing`;
+document.getElementById("h3_in_info_2").innerHTML = `Video Production`;
+document.getElementById("h3_in_info_3").innerHTML = `Curriculum Development`;
+//    [p]
+document.getElementById("p_in_info").innerHTML = `He excelled in programming language and has read several courses, including HTML, CSS, BootstrAP, JAVASCRIPT..`;
+document.getElementById("p_in_info_2").innerHTML = `I went through many projects but the strongest and most beautiful project I have put under the screen is the Cafena project. I am also very proud of these projects that I have produced.`;
+document.getElementById("p_in_info_3").innerHTML = `I obtained a sixth grade certificate with a grade of 85 and a third average certificate with an average of 83, and I am excellent in programming.`;
+document.getElementById("p_in_projects_info").innerHTML = `room sale website`;
+document.getElementById("p_in_projects_info_2").innerHTML = `Full page of BootStrap`;
+document.getElementById("p_in_projects_info_3").innerHTML = `A site to sell muay thai products`;
+document.getElementById("p_in_projects_info_4").innerHTML = `fake site`;
+document.getElementById("p_in_projects_info_5").innerHTML = `human rights website`;
+document.getElementById("p_in_projects_info_6").innerHTML = `coffee shop`;
+//    [span]
+document.getElementById("span_in_projects").innerHTML = `The first project`;
+document.getElementById("span_in_projects_2").innerHTML = `The second project`;
+document.getElementById("span_in_projects_3").innerHTML = `third project`;
+document.getElementById("span_in_projects_4").innerHTML = `Fourth Project`;
+document.getElementById("span_in_projects_5").innerHTML = `Fifth project`;
+document.getElementById("span_in_projects_6").innerHTML = `Sixth Project`;
